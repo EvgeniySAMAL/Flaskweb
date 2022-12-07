304cb890d56af1489e462639bcd1a581603bee2a
Samal <evgeniy.samal@mail.ru>
2022-12-07 17:18:02 +0300

    добавлен СSS файл

--- client.py
+++ client.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def index():
-    return render_template("blog/index.html")
+    return render_template("index.html")
 
 
 @app.route("/about")
 def about():
-    return render_template("blog/about.html")
+    return render_template("about.html")
 
 
 @app.route("/stadies")
